f4c9a7823f493619e29a9637a3eaa2d39b9932c3
Ishan Upamanyu <ishan.upamanyu@algonomy.com>
2022-03-08 02:07:49 +0530

    Changed to dismax query parser

--- WikiSearcher/src/main/java/com/wikisearchengine/wikisearcher/service/SolrSearchService.java
+++ WikiSearcher/src/main/java/com/wikisearchengine/wikisearcher/service/SolrSearchService.java
@@ -44,17 +44,23 @@ public class SolrSearchService implements SearchService{
                 resultsPerPage = maxResultsPerPageInt;
             }
 
-            String solrQuery = String.format("title_t:%s OR body_t:%s", query, query);
+
+            String solrQuery = buildSolrQueryDismax(query);
             final Map<String, String> queryParamMap = new HashMap<>();
-            queryParamMap.put("q", solrQuery);
+            queryParamMap.put("q", query);
             queryParamMap.put("start", String.valueOf(pageNum * resultsPerPage));
             queryParamMap.put("rows", String.valueOf(resultsPerPage));
+            queryParamMap.put("hl", "on");
+            queryParamMap.put("hl.fl", "body_t");
+            queryParamMap.put("defType", "dismax");
+            queryParamMap.put("qf", "title_t^20 body_t^0.5");
             MapSolrParams queryParams = new MapSolrParams(queryParamMap);
 
 
             final QueryResponse response = solrClient.query(enWikiCollectionName, queryParams);
 
             final SolrDocumentList documents = response.getResults();
+            Map<String, Map<String, List<String>>>highlights = response.getHighlighting();
 
             List<SearchResult> results = new ArrayList<>();
             for (SolrDocument document : documents) {
@@ -62,13 +68,48 @@ public class SolrSearchService implements SearchService{
                 final String url = "https://en.wikipedia.org/wiki?curid=" + id;
                 final String title = (String) document.getFirstValue("title_t");
                 final String body = (String) document.getFirstValue("body_t");
+                String highlight = null;
+                final Map<String, List<String>> highlightForDoc = highlights.get(id);
+                if(highlightForDoc != null){
+                    List<String> bodyHighlights = highlightForDoc.get("body_t");
+                    if(bodyHighlights != null && bodyHighlights.size() > 0){
+                        highlight = bodyHighlights.get(0);
+                    }
+                }
+
+                String desc = highlight == null ? body == null? "": body : highlight;
                 final Date date = (Date) document.getFirstValue("date_dt");
-                results.add(new SearchResult(title, url, body, SOLR_DATE_FORMAT.format(date.toInstant())));
+                results.add(new SearchResult(title, url, desc, SOLR_DATE_FORMAT.format(date.toInstant())));
             }
             return new SearchResponse(documents.getNumFound(), results);
         }catch (Exception e){
             throw new RuntimeException("An unexpected error occurred.", e);
         }
+    }
+
+    private String buildSolrQuery(String query) {
+        String words[] = null;
+        if(query != null){
+             words = query.split(" ");
+        }else {
+           throw new RuntimeException("Query should not be null.");
+        }
+        StringBuilder solrQuery = new StringBuilder();
+        for(String word: words){
+            solrQuery.append("title_t:"+word+"^10.0 OR ");
+        }
+
+        for(String word: words){
+            solrQuery.append("body_t:"+word+"^0.5 OR ");
+        }
+
+        solrQuery.delete(solrQuery.length() - 3, solrQuery.length());
+
+        return solrQuery.toString();
+    }
+
+    private String buildSolrQueryDismax(String query){
+        return String.format("title_t:%s OR body_t:%s", query, query);
     }
 
 
